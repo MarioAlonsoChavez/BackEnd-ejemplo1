295da6e5275ebd210c9219ca8920ab5139da77f5
mario <mario.alonso.chavez.s@gmail.com>
2022-10-24 18:09:10 -0500

    Agregar archivos de proyecto.

---
+++ BackEnd-ejemplo1/Controllers/CalculosBasController.cs
@@ -0,0 +1,49 @@
+﻿using BackEnd_ejemplo1.Models;
+using BackEnd_ejemplo1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_ejemplo1.Controllers
+{
+    public class CalculosBasController : Controller
+    {
+        // GET: CalculosBas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma(string Num1, string Num2)
+        {
+            int res =Convert.ToInt16(Num1) + Convert.ToInt16(Num2);
+
+            ViewBag.Res = Convert.ToString(res);
+
+            return View(res);
+        }
+        public ActionResult Operas()
+        {
+            OperasBas op = new OperasBas();
+            return View(op);
+        }
+        [HttpPost]
+
+        public ActionResult Operas(OperasBas op)
+        {
+            op.Suma();
+            //ViewBag.Res=op.Res;
+            return View(op);
+        }
+
+        public ActionResult MostrarPeliculas()
+        {
+            var peliculaService = new PeliculaService();
+            var model = peliculaService.ObtenerPelicula();
+
+            return View(model);
+        } 
+    }
+}
---
+++ BackEnd-ejemplo1/Services/PeliculaService.cs
@@ -0,0 +1,36 @@
+﻿using BackEnd_ejemplo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_ejemplo1.Services;
+
+
+public class PeliculaService
+{
+    public Peliculas()
+    {
+        public List<Peliculas> ObtenerPelicula()
+        {
+            var pelicula1 = new Peliculas()
+            {
+                Titulo = "SuperMan",
+                Duracion = 150,
+                Pais = "USA",
+                Publicacion = new DateTime(2012, 12, 11)
+            };
+
+            var pelicula2 = new Peliculas()
+            {
+                Titulo = "SpiderMan",
+                Duracion = 180,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 05, 02)
+            };
+
+            return new List<Peliculas> { pelicula1, pelicula2 };
+        }
+    }
+}
+}

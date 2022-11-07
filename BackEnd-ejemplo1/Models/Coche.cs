738fc15646e87153c32b9b13b7bc9a739e37cbaf
MarioAlonsoChavez <113148681+MarioAlonsoChavez@users.noreply.github.com>
2022-11-07 12:34:37 -0600

    Add files via upload

---
+++ BackEnd-ejemplo1/Models/Coche.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_ejemplo1.Models
+{
+    public class Coche
+    {
+        public string IdCoche { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Propietario { get; set; }
+
+        public Coche(string IdCoche, string Marca, string Modelo, string Propietario) //constructor
+        {
+            this.IdCoche =  IdCoche;
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+            this.Propietario = Propietario;
+        }
+    }
+}
